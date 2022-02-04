338e87840801a11e9a8fbd5dc843b3b7310717b2
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2022-02-04 17:08:50 +0000

    First Commit
    This is my fist commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+
+
+function App() {
+
+  const name = 'Brad'
+  const x = true
+
+
+  return (
+    <div className="container">
+      <Header title='Hello' />
+    </div>
+  );
+}
+
+export default App;
