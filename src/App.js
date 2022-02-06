0fb14b33e257ee3c305be561458574eb50ab4f29
stephen1cowley <98669276+stephen1cowley@users.noreply.github.com>
2022-02-06 18:47:07 +0000

    Images successful

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import Headerr from './components/Header'
 import Hia from './components/Header2'
 import React, { useState, useEffect } from 'react';
+import logo19 from './logo192.png';
+
+console.log(logo19);
 
 
 function App() {
-
   const [count, setCount] = useState(0);
 
   useEffect(() => {
     const fetchTasks = async () => {
       const res = await fetch('http://localhost:5000/stuff')
-
       const data = await res.json()
-
       return data
-
     }
 
     fetchTasks()
 
   }, [])
-
-
-
-
-
 
   return (
     <div className="container">
@@ -34,6 +28,9 @@ function App() {
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
+      <img src={logo19} alt="Logo" />
+      <img src={logo19} alt="Logo" />
+      <img src={logo19} alt="Logo" />
     </div>
   );
 }
